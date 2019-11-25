9944b7f4755a8a9f518a0b130cbe825fd90f0a6c
Sebastien Danthinne <sdanthinne@gmail.com>
2019-11-25 14:19:37 -0800

    split some of the CUDecoder up

--- sources_1/new/CUDecoder.sv
+++ sources_1/new/CUDecoder.sv
@@ -1,11 +1,8 @@
-module CUDecoder(WB_IR, WB_IR_EN, MEM_IR, MEM_IR_EN, EXE_IR, EXE_IR_EN, DEC_IR, DEC_IR_EN, BR_LT, BR_EQ, BR_LTU, ALU_FUNC, ALU_SRCA, ALU_SRCB, PCSOURCE, RF_WR_SEL);
-    input [31:0] WB_IR, MEM_IR, EXE_IR, DEC_IR;
-    input BR_LT, BR_EQ, BR_LTU;
-    output logic [3:0] ALU_FUNC;
-    output logic ALU_SRCA, REG_WR_EN, CSR_WRITE, MEM_READ2, MEM_SIGN;
-    output logic CLEAR;
-    output logic [1:0] ALU_SRCB, RF_WR_SEL, MEM_SIZE;
-    output logic [2:0] PC_SOURCE;
+module Decode_Decoder(DEC_IR,ALU_SRCA,ALU_SRCB);
+    input [31:0] DEC_IR;
+    output logic [1:0] ALU_SRCB;
+    output logic ALU_SRCA;
+    logic [6:0] DEC_OPCODE;
 
     typedef enum logic [6:0] {
     LUI = 7'b0110111,
@@ -19,42 +16,10 @@ module CUDecoder(WB_IR, WB_IR_EN, MEM_IR, MEM_IR_EN, EXE_IR, EXE_IR_EN, DEC_IR, 
     OP = 7'b0110011,
     SYSTEM = 7'b1110011
     } opcode_t;
-    opcode_t EXEC_OPCODE;
 
     assign DEC_OPCODE = opcode_t'(DEC_IR[6:0]);
-    assign EXEC_OPCODE = opcode_t'(EXE_IR[6:0]);
-    assign MEM_OPCODE = opcode_t'(MEM_IR[6:0]);
-    assign WB_OPCODE = opcode_t'(WB_IR[6:0]);
-
-    typedef enum logic [2:0] {
-    BEQ = 3'b000,
-    BNE = 3'b001,
-    BLT = 3'b100,
-    BGE = 3'b101,
-    BLTU = 3'b110,
-    BGEU = 3'b111
-    } func3_t;
-    func3_t func3_exe;
-
-    assign func3_exe = func3_t'(EXE_IR[14:12]);
-    assign func3_wb = func3_t'(WB_IR[14:12]);
-
-    assign func7 = EXE_IR[31:25];
-
-    always_comb
-    begin
-
-      PC_SOURCE = 0; // PC SIGNAL
-
-      CLEAR = 0; // IR REGISTER SIGNAL
-
-      REG_WR_EN = 0; CSR_WRITE = 0; RF_WR_EN = 0; ALU_SRCA = 0; ALU_SRCB = 0; // DECODE STAGE SIGNALS
-
-      ALU_FUNC = 0; // EXECUTE STAGE SIGNAL
-
-      MEM_READ2 = (MEM_OPCODE == LOAD) ? 1:0; MEM_WRITE2 = (MEM_OPCODE == STORE) ? 1:0; MEM_SIGN = MEM_IR[14]; MEM_SIZE = MEM_IR[13:12]; // MEMORY STAGE SIGNALS
-
-      case (DEC_OPCODE) // Handles ALU_SRCA | ALU_SRCB
+    
+    case (DEC_OPCODE) // Handles ALU_SRCA | ALU_SRCB
         LUI:
           begin
               ALU_SRCA = 1;
@@ -76,9 +41,29 @@ module CUDecoder(WB_IR, WB_IR_EN, MEM_IR, MEM_IR_EN, EXE_IR, EXE_IR_EN, DEC_IR, 
           begin
             ALU_SRCA = 0; ALU_SRCB = 0;
           end
-      endcase
+    endcase
+endmodule
 
-      case (EXEC_OPCODE) // HANDLES PC_SOURCE | ALU_FUNC
+module Execute_Decoder(EXE_IR,ALU_FUNC,CLEAR);
+    input [31:0] EXE_IR;
+    logic [7:0] EXEC_OPCODE;
+
+    typedef enum logic [6:0] {
+    LUI = 7'b0110111,
+    AUIPC = 7'b0010111,
+    JAL = 7'b1101111,
+    JALR = 7'b1100111,
+    BRANCH = 7'b1100011,
+    LOAD = 7'b0000011,
+    STORE = 7'b0100011,
+    OP_IMM = 7'b0010011,
+    OP = 7'b0110011,
+    SYSTEM = 7'b1110011
+    } opcode_t;
+
+    assign EXEC_OPCODE = opcode_t'(EXE_IR[6:0]);
+
+    case (EXEC_OPCODE) // HANDLES PC_SOURCE | ALU_FUNC
         LUI:
           begin
             ALU_FUNC = 9;
@@ -154,6 +139,69 @@ module CUDecoder(WB_IR, WB_IR_EN, MEM_IR, MEM_IR_EN, EXE_IR, EXE_IR_EN, DEC_IR, 
             PC_SOURCE = 0; ALU_FUNC = 0;
           end
       endcase
+
+endmodule
+
+
+module CUDecoder(WB_IR, WB_IR_EN, MEM_IR, MEM_IR_EN, EXE_IR, EXE_IR_EN, DEC_IR, DEC_IR_EN, BR_LT, BR_EQ, BR_LTU, ALU_FUNC, ALU_SRCA, ALU_SRCB, PCSOURCE, RF_WR_SEL);
+    input [31:0] WB_IR, MEM_IR, EXE_IR, DEC_IR;
+    input BR_LT, BR_EQ, BR_LTU;
+    output logic [3:0] ALU_FUNC;
+    output logic ALU_SRCA, REG_WR_EN, CSR_WRITE, MEM_READ2, MEM_SIGN;
+    output logic CLEAR;
+    output logic [1:0] ALU_SRCB, RF_WR_SEL, MEM_SIZE;
+    output logic [2:0] PC_SOURCE;
+
+    typedef enum logic [6:0] {
+    LUI = 7'b0110111,
+    AUIPC = 7'b0010111,
+    JAL = 7'b1101111,
+    JALR = 7'b1100111,
+    BRANCH = 7'b1100011,
+    LOAD = 7'b0000011,
+    STORE = 7'b0100011,
+    OP_IMM = 7'b0010011,
+    OP = 7'b0110011,
+    SYSTEM = 7'b1110011
+    } opcode_t;
+    opcode_t EXEC_OPCODE;
+
+   
+    
+    assign MEM_OPCODE = opcode_t'(MEM_IR[6:0]);
+    assign WB_OPCODE = opcode_t'(WB_IR[6:0]);
+
+    typedef enum logic [2:0] {
+    BEQ = 3'b000,
+    BNE = 3'b001,
+    BLT = 3'b100,
+    BGE = 3'b101,
+    BLTU = 3'b110,
+    BGEU = 3'b111
+    } func3_t;
+    func3_t func3_exe;
+
+    assign func3_exe = func3_t'(EXE_IR[14:12]);
+    assign func3_wb = func3_t'(WB_IR[14:12]);
+
+    assign func7 = EXE_IR[31:25];
+
+    always_comb
+    begin
+
+      PC_SOURCE = 0; // PC SIGNAL
+
+      CLEAR = 0; // IR REGISTER SIGNAL
+
+      REG_WR_EN = 0; CSR_WRITE = 0; RF_WR_EN = 0; ALU_SRCA = 0; ALU_SRCB = 0; // DECODE STAGE SIGNALS
+
+      ALU_FUNC = 0; // EXECUTE STAGE SIGNAL
+
+      MEM_READ2 = (MEM_OPCODE == LOAD) ? 1:0; MEM_WRITE2 = (MEM_OPCODE == STORE) ? 1:0; MEM_SIGN = MEM_IR[14]; MEM_SIZE = MEM_IR[13:12]; // MEMORY STAGE SIGNALS
+
+     
+
+      
 
       PC_SOURCE = (INT_TAKEN) ? 4:PC_SOURCE; // Haven't determined how to handle intTaken
       //int_taken will be set in execute state, and the interrupts will be triggered from then on. 
