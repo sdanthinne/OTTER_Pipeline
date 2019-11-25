f16b005865386a4c07d69b334b68abfe2fc9ae53
Alexander Knapen <alexander.w.knapen@gmail.com>
2019-11-25 14:09:18 -0800

    Correct outputs for data resolution

--- sources_1/new/DataResolution.sv
+++ sources_1/new/DataResolution.sv
@@ -5,8 +5,7 @@ module DataResolution(
   input hzd_in,
   input [31:0] decodeIR_out,
   input [31:0] executeIR_out,
-  output logic executeIR_en, decodeIR_en, pc_write, hzd_out, mem_read1,
-  output logic [31:0] mem_out
+  output logic reg_en, pc_write, hzd_out
 );
 
 // decodeIR_out is the output of the decode register
@@ -21,16 +20,16 @@ module DataResolution(
         if ((decodeIR_out[6:0] == 7'b1100011 || decodeIR_out[6:0] == 7'b0100011
             || decodeIR_out[6:0] == 7'b0110011) && executeIR_out[11:7] == decodeIR_out[24:20]) // opcodes with rs2; checks if rs2 = rd
           begin
-            executeIR_en = 0; decodeIR_en = 0; pc_write = 0; mem_out = 32'h00000013; mem_read1 = 0; hzd = 1; // halt the flow of data from new instructions
+            reg_en = 0; pc_write = 0; hzd_out = 1; // halt the flow of data from new instructions
           end
         if (decodeIR_out[2:0] != 3'b111 && decodeIR_out[19:15] == executeIR_out[11:7]) // opcodes with rs1; checks if rs1 = rd
           begin
-            executeIR_en = 0; decodeIR_en = 0; pc_write = 0; mem_out = 32'h00000013; mem_read1 = 0; hzd = 1; // halt the flow of data from new instructions
+            reg_en = 0; pc_write = 0; hzd_out = 1; // halt the flow of data from new instructions
           end
       end
       if (hzd_in == 1)
       begin
-        executeIR_en = 1; decodeIR_en = 1; pc_write = 1; mem_read1 = 1; hzd_out = 0;
+        reg_en = 1; pc_write = 1; hzd_out = 0;
       end
   end
 endmodule
